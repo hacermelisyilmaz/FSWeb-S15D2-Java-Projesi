49a788ac00d364fc78b524d4e4052465ce16e1d8
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-28 12:36:30 +0300

    Variables declared

--- src/com/workintech/firstchallenge/modals/Task.java
+++ src/com/workintech/firstchallenge/modals/Task.java
@@ -1,5 +1,12 @@
 package com.workintech.firstchallenge.modals;
 
-public class Task {
+import com.workintech.firstchallenge.enums.Priority;
+import com.workintech.firstchallenge.enums.Status;
 
+public class Task {
+    private String project;
+    private String description;
+    private String asignee;
+    Priority priority;
+    Status status;
 }
