bd995fea9fd131cf5ffe78e43886dd74ab2ba637
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-28 18:19:59 +0300

    Test cases logged

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,55 @@ import com.workintech.firstchallenge.modals.Task;
 import com.workintech.firstchallenge.modals.TaskData;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         Task task1 = new Task("project1", "creating classes");
         Task task2 = new Task("project1", "declaring variables", "ann", Priority.MED, Status.IN_QUEUE);
         Task task3 = new Task("project1", "creating interface", "bob", Priority.MED, Status.IN_PROGRESS);
-        Task task4 = new Task("project2", "mapping out inheritance", "carol", Priority.LOW, Status.ASSIGNED);
+        Task task4 = new Task("project1", "creating interface", "carol", Priority.LOW, Status.ASSIGNED);
         Task task5 = new Task("project2", "creating abstract classes", "carol", Priority.MED, Status.IN_PROGRESS);
 
-        TaskData taskData1 = new TaskData();
-        taskData1.setAnnsTasks(new HashSet<>() {{
-            add(task2);
-        }});
-        taskData1.setBobsTasks(new HashSet<>() {{
-            add(task3);
-        }});
-        taskData1.setCarolsTasks(new HashSet<>() {{
-            add(task4);
-            add(task5);
-        }});
+        Set<Task> annsTasks = new LinkedHashSet<>();
+        annsTasks.add(task2);
+
+        Set<Task> bobsTasks = new LinkedHashSet<>();
+        bobsTasks.add(task3);
+
+        Set<Task> carolsTasks = new LinkedHashSet<>();
+        carolsTasks.add(task4);
+        carolsTasks.add(task5);
+
+        Set<Task> allTasks = new LinkedHashSet<>();
+        allTasks.add(task1);
+        allTasks.addAll(annsTasks);
+        allTasks.addAll(bobsTasks);
+        allTasks.addAll(carolsTasks);
+
+        TaskData taskData = new TaskData(annsTasks, bobsTasks, carolsTasks);
+
 
         System.out.println("**ALL TASKS**");
-        for (Task task: taskData1.getTasks("all")) {
-            System.out.println(task);
-        }
+        Set<Task> all = taskData.getTasks("all");
+        System.out.println(all);
+
+        System.out.println("**TASKS ON ASSIGNEES**");
+        System.out.println("ANN: " + taskData.getTasks("ann"));
+        System.out.println("BOB: " + taskData.getTasks("bob"));
+        System.out.println("CAROL: " + taskData.getTasks("carol"));
+
+        System.out.println("**UNASSIGNED TASKS**");
+        Set<Task> unassignedTasks = taskData.getDifference(allTasks, taskData.getTasks("all"));
+        System.out.println(unassignedTasks);
+
+        System.out.println("**MULTIPLE ASSIGNEE**");
+        Set<Task> intAnnBob =  taskData.getIntersect(annsTasks, bobsTasks);
+        System.out.println("ANN & BOB: " + intAnnBob);
+        Set<Task> intAnnCarol = taskData.getIntersect(annsTasks, carolsTasks);
+        System.out.println("ANN & CAROL: " + intAnnCarol);
+        Set<Task> intBobCarol =  taskData.getIntersect(bobsTasks, carolsTasks);
+        System.out.println("BOB & CAROL: " + intBobCarol);
     }
 }
