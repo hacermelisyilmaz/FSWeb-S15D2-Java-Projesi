181b05f633d61e68b367ad9bb6adcc1789696d86
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-28 12:40:14 +0300

    Three Sets as variables are created

--- src/com/workintech/firstchallenge/modals/TaskData.java
+++ src/com/workintech/firstchallenge/modals/TaskData.java
@@ -1,4 +1,9 @@
 package com.workintech.firstchallenge.modals;
 
+import java.util.Set;
+
 public class TaskData {
+    private Set<Task> annsTasks;
+    private Set<Task> bobsTasks;
+    private Set<Task> carolsTasks;
 }
