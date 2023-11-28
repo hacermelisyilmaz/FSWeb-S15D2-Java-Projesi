b19b06bc81614f590a6aee6fd9efddacafdc75e3
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-28 14:32:50 +0300

    getTasks() method

--- src/com/workintech/firstchallenge/modals/TaskData.java
+++ src/com/workintech/firstchallenge/modals/TaskData.java
@@ -1,9 +1,32 @@
 package com.workintech.firstchallenge.modals;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class TaskData {
     private Set<Task> annsTasks;
     private Set<Task> bobsTasks;
     private Set<Task> carolsTasks;
+
+    public Set<Task> getTasks(String assignee) {
+        Set<Task> returnTasks = null;
+        switch (assignee) {
+            case "ann": {
+                returnTasks = annsTasks;
+            }
+            case "bob": {
+                returnTasks = bobsTasks;
+            }
+            case "carol": {
+                returnTasks = carolsTasks;
+            }
+            case "all": {
+                returnTasks.addAll(annsTasks);
+                returnTasks.addAll(bobsTasks);
+                returnTasks.addAll(carolsTasks);
+            }
+            default: System.out.println("Please select a viable assignee.");
+        }
+        return returnTasks;
+    }
 }
