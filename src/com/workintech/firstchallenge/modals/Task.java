3c8df0c0ed857fd9c5aea795013a6b8732f76d35
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-28 12:38:46 +0300

    equals() and hashCode() methods created

--- src/com/workintech/firstchallenge/modals/Task.java
+++ src/com/workintech/firstchallenge/modals/Task.java
@@ -3,10 +3,24 @@ package com.workintech.firstchallenge.modals;
 import com.workintech.firstchallenge.enums.Priority;
 import com.workintech.firstchallenge.enums.Status;
 
+import java.util.Objects;
+
 public class Task {
     private String project;
     private String description;
     private String asignee;
     Priority priority;
     Status status;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Task task)) return false;
+        return Objects.equals(project, task.project) && Objects.equals(description, task.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(project, description);
+    }
 }
